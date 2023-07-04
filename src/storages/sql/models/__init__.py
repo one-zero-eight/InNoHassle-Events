8bdb32c0cbb4cf216e6e6d58ad82371d8f5183b7
Saveliy <s.lekhtin@innopolis.university>
2023-07-04 16:44:15 +0300

    feat: add tags model

--- src/storages/sql/models/__init__.py
+++ src/storages/sql/models/__init__.py
@@ -2,7 +2,8 @@ from src.storages.sql.models.base import Base
 
 from src.storages.sql.models.users import User
 from src.storages.sql.models.event_groups import EventGroup, UserXFavorite, UserXGroup
+from src.storages.sql.models.tags import Tag, TagXEventGroup
 
-all_models = [User, EventGroup, UserXFavorite, UserXGroup]
+all_models = [User, EventGroup, UserXFavorite, UserXGroup, Tag, TagXEventGroup]
 
 __all__ = [*all_models, Base]
---
+++ src/storages/sql/models/tags.py
@@ -0,0 +1,37 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.storages.sql.models import Base
+from src.storages.sql.models.event_groups import EventGroup
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    description: Mapped[str] = mapped_column(nullable=True)
+
+    event_group_association: Mapped[list["TagXEventGroup"]] = relationship(
+        back_populates="tag",
+        cascade="all, delete-orphan",
+    )
+
+    event_group: Mapped[list["EventGroup"]] = association_proxy(
+        "event_group_association",
+        "event_group",
+        creator=lambda event_group: TagXEventGroup(event_group=event_group),
+    )
+
+
+class TagXEventGroup(Base):
+    __tablename__ = "tags_x_event_groups"
+    tag_id: Mapped[int] = mapped_column(ForeignKey("tags.id"), primary_key=True)
+    event_group_id: Mapped[int] = mapped_column(
+        ForeignKey("groups.id"), primary_key=True
+    )
+
+    tag: Mapped["Tag"] = relationship("Tag", back_populates="event_group_association")
+    event_group: Mapped[EventGroup] = relationship(lazy="joined")
